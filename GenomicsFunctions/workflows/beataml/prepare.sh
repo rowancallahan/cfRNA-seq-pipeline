b3c73a95729e9e6d35f9bbce85d453ea3d86e924
Joey Estabrook <estabroj@ohsu.edu>
2018-01-18 09:38:39 -0800

    Removed unnecessary functions and directories

--- GenomicsFunctions/workflows/beataml/prepare.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-PROJ="/home/users/letaw/lustre1/projs/beataml2"
-export PATH="/opt/installed"
-
-echo $PATH
-/mnt/lustre1/CompBio/bin/rsem-prepare-reference --transcript-to-gene-map $PROJ/gene_trans.tsv $PROJ/known_ensembl_trans.fasta $PROJ/hg19_2
