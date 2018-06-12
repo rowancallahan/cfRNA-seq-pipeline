fb395d0569663c0ad225193b91ebeb249e80d5ef
Joseph Estabrook <JEstabrook@users.noreply.github.com>
2018-06-12 15:01:39 -0700

    Adding submit snakemake script with --mem flag

--- utils/submit_snakemake.sh
+++ utils/submit_snakemake.sh
@@ -2,4 +2,4 @@
 #SBATCH --time 35:00:00
 #SBATCH --partition exacloud
 
-snakemake -j 300 --cluster-config cluster.json --cluster "sbatch -p {cluster.partition} -N {cluster.N}  -t {cluster.t} -o {cluster.o} -e {cluster.e} -J {cluster.J} -c {cluster.c}" -s Snakefile 2> logs/all_%j.log
+snakemake -j 300 --cluster-config cluster.json --cluster "sbatch -p {cluster.partition} -N {cluster.N}  -t {cluster.t} -o {cluster.o} -e {cluster.e} -J {cluster.J} -c {cluster.c} --mem {cluster.mem}" -s Snakefile 2> logs/all_%j.log
