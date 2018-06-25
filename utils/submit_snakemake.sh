efc293fded263fa4cf0dc3079b23b34d8ea96bc9
Joseph Estabrook <JEstabrook@users.noreply.github.com>
2018-06-25 14:21:20 -0700

    add --use-conda flag for sbatch submit

--- utils/submit_snakemake.sh
+++ utils/submit_snakemake.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/bash
 #SBATCH --time 35:00:00
 #SBATCH --partition exacloud
+#SBATCH --job-name omic-wf-submit
 
-snakemake -j --cluster-config cluster.json --cluster "sbatch -p {cluster.partition} -N {cluster.N}  -t {cluster.t} -o {cluster.o} -e {cluster.e} -J {cluster.J} -c {cluster.c} --mem {cluster.mem}" -s Snakefile 2> logs/all_%j.log
+snakemake --use-conda -j --cluster-config cluster.json --cluster "sbatch -p {cluster.partition} -N {cluster.N}  -t {cluster.t} -o {cluster.o} -e {cluster.e} -J {cluster.J} -c {cluster.c} --mem {cluster.mem}" -s Snakefile
