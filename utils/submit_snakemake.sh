65b4635df3b46babaa9c6e6e943185d921cfc9e5
Joseph Estabrook <JEstabrook@users.noreply.github.com>
2018-06-29 16:06:20 -0700

    Modified output log to be placed in snakemake log files, incorporated --use-conda flag to snakemake slurm submission

--- utils/submit_snakemake.sh
+++ utils/submit_snakemake.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/bash
 #SBATCH --time 35:00:00
 #SBATCH --partition exacloud
-#SBATCH --job-name omic-wf-submit
+#SBATCH --job-name workflow_submission
+#SBATCH --output=logs/workflow_submission_%j.log
 
-snakemake --use-conda -j --cluster-config cluster.json --cluster "sbatch -p {cluster.partition} -N {cluster.N}  -t {cluster.t} -o {cluster.o} -e {cluster.e} -J {cluster.J} -c {cluster.c} --mem {cluster.mem}" -s Snakefile
+source activate omic-qc-wf
+snakemake -j 100 --use-conda --cluster-config cluster.json --cluster "sbatch -p {cluster.partition} -N {cluster.N}  -t {cluster.t} -o {cluster.o} -e {cluster.e} -J {cluster.J} -c {cluster.c} --mem {cluster.mem}" -s Snakefile
