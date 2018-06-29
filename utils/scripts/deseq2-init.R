3d4a52240e04ff8b08273aaaaa69a7a0df2764c0
Joseph Estabrook <JEstabrook@users.noreply.github.com>
2018-06-29 16:59:06 -0700

    adding initialization script for deseq2, saves dds object

---
+++ utils/scripts/deseq2-init.R
@@ -0,0 +1,37 @@
+
+library("DESeq2")
+args = commandArgs(trailingOnly = TRUE)
+
+threads = args[1]
+counts = args[2]
+params = args[3]
+output = args[4]
+design = args[5]
+row_names = args[6]
+
+parallel <- FALSE
+if (threads > 1) {
+    library("BiocParallel")
+    # setup parallelization
+    register(MulticoreParam(snakemake@threads))
+    parallel <- TRUE
+}
+
+# colData and countData must have the same sample order, but this is ensured
+# by the way we create the count matrix
+cts <- read.table(counts, header=TRUE, row.names=1, sep="\t")
+coldata <- read.table(params, header=TRUE, row.names=row_names,sep="\t")
+
+coldata <- coldata[colnames(cts),]
+cts <- cts[, rownames(coldata)]
+
+dds <- DESeqDataSetFromMatrix(countData=cts,
+                              colData=coldata,
+                              design=~ design)
+
+# remove uninformative columns
+dds <- dds[ rowSums(counts(dds)) > 1, ]
+# normalization and preprocessing
+dds <- DESeq(dds, parallel=parallel)
+
+saveRDS(dds, file=output)
