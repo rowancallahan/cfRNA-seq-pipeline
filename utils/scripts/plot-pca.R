8c942089eeacb45f91d6eefbab126e22732186c3
Joseph Estabrook <JEstabrook@users.noreply.github.com>
2018-07-17 11:00:04 -0700

    Removing trailing command line arguments, supplementing with snakemake S4 object. Adding top variance genes heatmap.

--- utils/scripts/plot-pca.R
+++ utils/scripts/plot-pca.R
@@ -3,75 +3,104 @@ library("ggplot2")
 library("pheatmap")
 library("vsn")
 library("RColorBrewer")
+library("genefilter")
 
-rds = snakemake@input[['rds_object']]
-pca_plot = snakemake@output[['pca']]
-rld_out = snakemake@output[['rld_out']]
-labels = snakemake@params[['pca_labels']]
-sd_mean_plot =snakemake@output[['sd_mean_plot']]
-heatmap_plot =snakemake@output[['heatmap_plot']]
-distance_plot =snakemake@output[['distance_plot']]
-ggplot_pca =snakemake@output[['ggplot_pca']]
-panel_ma =snakemake@output[['panel_ma']]
+cat(sprintf(c('Working directory',getwd())))
 
-# load deseq2 data
-dds <- readRDS(rds)
+cat(sprintf('Setting parameters'))
 
-# obtain normalized counts
-rld <- rlog(dds, blind=FALSE)
-saveRDS(rld, file=rld_out)
+rds <- snakemake@input[['rds_object']]
+cat(sprintf(c('RDS: ',rds)))
 
+dds <- snakemake@input[['dds_object']]
+cat(sprintf(c('DDS: ',rds)))
 
-svg(pca_plot)
+pca_plot <- snakemake@output[['pca']]
+cat(sprintf(c('PCA plot: ',pca_plot)))
+
+labels <- snakemake@params[['pca_labels']]
+cat(sprintf(c('PCA Labels: ',labels)))
+
+sd_mean_plot <- snakemake@output[['sd_mean_plot']]
+cat(sprintf(c('SD Mean plot: ',sd_mean_plot,'\n')))
+
+heatmap_plot <- snakemake@output[['heatmap_plot']]
+cat(sprintf(c('Heatmap plot: ',heatmap_plot,'\n')))
+
+distance_plot <- snakemake@output[['distance_plot']]
+cat(sprintf(c('Distance plot: ',distance_plot,'\n')))
+
+panel_ma <- snakemake@output[['panel_ma']]
+cat(sprintf(c('panel MA plot: ',panel_ma,'\n')))
+
+var_heat <- snakemake@output[['var_heat']]
+cat(sprintf(c('Top Variance Genes Heatmap: ',var_heat,'\n')))
+
+ggplot_pca_factor <- snakemake@output[['ggplot_pca_factor']]
+cat(sprintf(c('ggplot2 PCA factor plot: ', ggplot_pca_factor,'\n')))
+
+cat(sprintf('Load rlog DESeqTransform object'))
+rld <-readRDS(rds)
+
+cat(sprintf('Load dds DESeqTransform object'))
+dds <-readRDS(dds)
+
+pdf(pca_plot)
 plotPCA(rld, intgroup=labels)
 dev.off()
 
 # SD mean plot
-svg(sd_mean_plot)
+pdf(sd_mean_plot)
 meanSdPlot(assay(rld))
 dev.off()
 
 # Heatmap top 20 DE genes
-
+# TODO Add flexible/extensible filtering criteria to parameter configuration file for plotting utilities
 res <-results(dds)
 topGenes <- head(order(res$padj),20)
 
-df <- as.data.frame(colData(dds))
+df <- as.data.frame(colData(rld))
 
 
-svg(heatmap_plot)
-pheatmap(assay(rld)[topGenes,], cluster_rows=FALSE, show_rownames=FALSE,
-         cluster_cols=FALSE, annotation_col=df)
+pdf(heatmap_plot)
+pheatmap(assay(rld)[topGenes,], cluster_rows=T, fontsize=6,fontsize_row=6,fontsize_col=6,show_rownames=T,
+         cluster_cols=T, annotation_col=df[,c('Type','RNA_extracted_by','Lib_prep_date')],labels_col=as.character(df$SampleID))
 dev.off()
 
 # Heatmap of distances
 
-svg(distance_plot)
+pdf(distance_plot)
 sampleDists <- dist(t(assay(rld)))
 sampleDistMatrix <- as.matrix(sampleDists)
+
+# TODO allow for dynamic row naming conventions
 rownames(sampleDistMatrix) <- paste(rld$Type, rld$RNA_extracted_by, sep="-")
 colnames(sampleDistMatrix) <- NULL
 colors <- colorRampPalette( rev(brewer.pal(9, "Blues")) )(255)
-pheatmap(sampleDistMatrix,
+pheatmap(sampleDistMatrix, fontsize=5,
          clustering_distance_rows=sampleDists,
          clustering_distance_cols=sampleDists,
          col=colors)
 dev.off()
 
 # ggplot PCA
-svg(ggplot_pca)
+
 pcaData <- plotPCA(rld, intgroup=c("Type", "RNA_extracted_by"), returnData=TRUE)
+
+pdf(ggplot_pca_factor)
 percentVar <- round(100 * attr(pcaData, "percentVar"))
-ggplot(pcaData, aes(PC1, PC2, color=Type, shape=RNA_extracted_by)) +
+# TODO allow for dynamic color and shape naming conventions
+g<-ggplot(pcaData, aes(PC1, PC2, color=Type, shape=RNA_extracted_by)) +
   geom_point(size=3) +
   xlab(paste0("PC1: ",percentVar[1],"% variance")) +
   ylab(paste0("PC2: ",percentVar[2],"% variance")) +
   coord_fixed()
+g + theme_gray()
 dev.off()
 
 #panel ma plot
-svg(panel_ma)
-par(mfrow=c(2,2),mar=c(2,2,1,1))
+pdf(panel_ma)
+par(mfrow=c(2,2),mar=c(2,2,1,1) +0.1)
 ylim <- c(-2.5,2.5)
 resGA <- results(dds, lfcThreshold=.5, altHypothesis="greaterAbs")
 resLA <- results(dds, lfcThreshold=.5, altHypothesis="lessAbs")
@@ -82,5 +111,14 @@ plotMA(resGA, ylim=ylim); drawLines()
 plotMA(resLA, ylim=ylim); drawLines()
 plotMA(resG, ylim=ylim); drawLines()
 plotMA(resL, ylim=ylim); drawLines()
+mtext(resG@elementMetadata$description[[2]], outer=T, cex=.6,line=-1)
 dev.off()
 
+
+pdf(var_heat)
+topVarGenes <- head(order(rowVars(assay(rld)), decreasing = TRUE), 20)
+mat  <- assay(rld)[ topVarGenes, ]
+mat  <- mat - rowMeans(mat)
+anno <- as.data.frame(colData(rld))
+pheatmap(mat, annotation_col = anno[,c('Type','RNA_extracted_by','Lib_prep_date')],fontsize=6,)
+dev.off()
