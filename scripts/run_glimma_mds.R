5512c02b4fef97a2800a673c4640d6c194050c29
roskamsh <roskamsh@exanode-3-0.local>
2019-05-21 15:54:00 -0700

    updated number of genes to 1000

--- scripts/run_glimma_mds.R
+++ scripts/run_glimma_mds.R
@@ -13,4 +13,4 @@ dir.create(out_path)
 
 rds = readRDS(rds)
 groups.df = as.data.frame(colData(rds))
-glMDSPlot(rds, groups=groups.df,path=out_path,html=paste(project_id,'mds_plot',sep='.'),launch=FALSE)
+glMDSPlot(rds, top = 1000, groups=groups.df,path=out_path,html=paste(project_id,'mds_plot',sep='.'),launch=FALSE)
